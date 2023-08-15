6f6b036c71a3a90e09f3c9e669fea62dcf5c5b30
parisa kalaki <parisakalaki@parisas-MacBook-Air.local>
2023-08-15 17:35:27 +0300

    create new directories

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
 import GlobalStyles from "./styles/GlobalStyles";
 
-import Header from "./Header";
-import Sidebar from "./Sidebar";
-import Dashboard from "./dashboard";
+import Dashboard from "./pages/Dashboard";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Applayout from "./AppLayout";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <GlobalStyles />
-      <Header />
-      <Sidebar />
-      <Dashboard />
-    </div>
+      <Routes>
+        <Route element={<Applayout />}>
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/bookings" element={<Bookings />} />
+          <Route path="/cabins" element={<Cabins />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/settings" element={<Settings />} />
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Bookings.jsx
@@ -0,0 +1,9 @@
+function Bookings() {
+  return (
+    <div>
+      <span>Bookings</span>
+    </div>
+  );
+}
+
+export default Bookings;
