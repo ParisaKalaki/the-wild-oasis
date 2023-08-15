5025a4b2b0ee8f40e54a8128c15cbe945810c993
parisa kalaki <parisakalaki@parisas-MacBook-Air.local>
2023-08-15 15:54:50 +0300

    creating MainNav in sidebar

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,11 +1,14 @@
+/* eslint-disable no-unused-vars */
+
 import Logo from "./Logo";
+import Mainnav from "./Mainnav";
 
 function Sidebar() {
   return (
-    <div>
-      Sidebar
+    <nav>
       <Logo />
-    </div>
+      <Mainnav />
+    </nav>
   );
 }
 
