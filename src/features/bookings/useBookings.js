1087e82e38f639d68f9007aebf96db8bfb4a2772
parisa kalaki <parisakalaki@parisas-MacBook-Air.local>
2023-10-05 11:39:47 +0300

    prefetching with React Query

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,8 +1,10 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
 import { useSearchParams } from "react-router-dom";
+import { PAGE_SIZE } from "../../utils/constants";
 
 export function useBookings() {
+  const queryClient = useQueryClient();
   const [searchParams] = useSearchParams();
   //Filter
   const filterValue = searchParams.get("status");
@@ -15,7 +17,7 @@ export function useBookings() {
   const [field, direction] = sortByRow.split("-");
   const sortBy = { field, direction };
 
-  //pagination
+  //query
   const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
 
   const {
@@ -23,8 +25,23 @@ export function useBookings() {
     isLoading,
     error,
   } = useQuery({
-    queryFn: () => getBookings({ filter, sortBy, page }),
     queryKey: ["bookings", filter, sortBy, page], // whenever filter changes, then React Query will refetch the data
+    queryFn: () => getBookings({ filter, sortBy, page }),
   });
+
+  //Pre-fetching: pre-fetch all data that we know might become necessary before we actually need that data to render it on the user interface
+  //in the context of pagination, we fetch the next page before it is displayed. It's make the user experience better
+  const pageCount = Math.ceil(count / PAGE_SIZE);
+  if (page < pageCount)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page + 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+    });
+  if (page > 1)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page - 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
+    });
+
   return { bookings, isLoading, error, count };
 }
