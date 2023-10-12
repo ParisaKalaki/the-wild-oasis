1e6828b2baaf0736437fafa171eb380434db88aa
parisa kalaki <parisakalaki@parisas-MacBook-Air.local>
2023-10-12 15:42:32 +0300

    added user logout

---
+++ src/features/authentication/useLogout.js
@@ -0,0 +1,17 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { logout as logoutApi } from "../../services/apiAuth";
+import { useNavigate } from "react-router-dom";
+
+export function useLogout() {
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
+  const { mutate: logout, isLoading } = useMutation({
+    mutationFn: logoutApi,
+    onSuccess: () => {
+      queryClient.removeQueries(), //remove user from cache
+        navigate("/login", { replace: true });
+    }, // erase the place that we were earlier
+  });
+  return { logout, isLoading };
+}
